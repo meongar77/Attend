503efbf1926f64e18b4e6fa0625c854166185445
Garou Simeon <meongar77@gmail.com>
2026-07-30 23:58:08 +0200

    created faculty services

---
+++ Core/Application/Services/ClasssSerives/ClasssService.cs
@@ -0,0 +1,32 @@
+using Application.DTOs;
+using Application.Interfaces;
+using Domain.Entities;
+namespace Application.Services.ClasssServices
+{
+    public class ClasssService:IClasssService
+    {
+        private readonly IClasss _classs;
+        public ClasssService(IClasss classs)
+        {
+            _classs=classs;
+        }
+        public async Task<List<GetClasssDTO>> GetAllClasssAsync()
+        {
+           return await _classs.GetAllClasssAsync();
+        }
+        public async Task AddClasssAsync(AddClasssDTO classs)
+        {
+            await _classs.AddClasssAsync(classs);
+        }
+        public async Task<GetClasssDTO?> GetClasssByIdAsync(int id)
+        {
+            return await _classs.GetClasssByIdAsync(id);
+        }
+        public async Task UpdateClasssAsync(UpdateClasssDTO classs)
+        {
+            await _classs.UpdateClasssAsync(classs);
+        }
+    }
+    
+}
+            
